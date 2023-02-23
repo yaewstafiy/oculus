616d4be495aaeaff4407974250ce9bd84f4670ac
yaewstafiy <stasik7771@gmail.com>
2023-02-24 00:16:33 +0300

    adaptive and more

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+  // TABS
+
   const tabsContent = document.querySelectorAll('.features-tabs__content-item'),
     tabsParent = document.querySelector('.features-tabs__btn'),
     tabs = document.querySelectorAll('.features-tabs__btn-item');
@@ -38,5 +40,71 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
+
+
+  // BURGER
+
+  const burgerButton = document.querySelector('.burger');
+  const overlay = document.querySelector('.overlay');
+  const headerLogo = document.querySelector('.header__logo');
+
+  function closeMenu() {
+    burgerButton.classList.remove('burger--active');
+    overlay.classList.remove('overlay--active');
+    document.body.style.overflow = '';
+  }
+
+  burgerButton.addEventListener('click', (e) => {
+    overlay.classList.toggle('overlay--active');
+    burgerButton.classList.toggle('burger--active');
+    overlay.prepend(headerLogo);
+
+    if (burgerButton.classList.contains('burger--active')) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  })
+
+  document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape' && overlay.classList.contains('overlay--active')) {
+      closeMenu();
+    }
+  });
+
+  // Добавление формы в меню
+
+  const newForm = document.createElement('form');
+  newForm.classList.add('menu-form');
+  newForm.innerHTML = `
+    <div class="menu-form__title">
+      Contact us
+    </div>
+    <input class="menu-form__input" type="email">
+    <input class="menu-form__input" type="tel">
+    <button class="menu-form__btn" type="submit">Send</button>
+  `;
+  overlay.append(newForm);
+
+  // Закрытие меню
+
+  // window.addEventListener('click', (e) => {
+  //   const target = e.target;
+  //   if (
+  //     target !== burgerButton &&
+  //     target !== overlay &&
+  //     target !== headerLogo
+  //   ) {
+  //     burgerButton.classList.remove('burger--active');
+  //     overlay.classList.remove('overlay--active');
+  //     document.body.style.overflow = '';
+  //   }
+  // });
+
+
+
+  // ПОТОМ НАДО ЗАКОНЧИТЬ, КАК ПОЙМУ КАК РЕШИТЬ ПРОБЛЕМУ
+
+
 });
 
